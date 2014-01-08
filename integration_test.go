209b499e6c39a21d537937cde78a1c61e2f3b426
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-01-08 11:48:36 +0100

    wip (simulation of simulation)

--- integration_test.go
+++ integration_test.go
@@ -4,8 +4,12 @@ package main
 
 import (
 	"bufio"
+	"io/ioutil"
 	"log"
-	"net"
+	"math/rand"
+	"os"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -15,97 +19,117 @@ import (
 const (
 	// Number of simulated automats with RFIDService
 	NUMCLIENTS = 100
+
 	// How long to run the integration test
-	DURATION = 2 * time.Second
+	DURATION = 200 * time.Second
+
 	// TCPServer addresss
 	HOST = "localhost:6666"
+
 	// File containing barcodes; one per line
-	ITEMSFILE = "items.txt"
+	ITEMSFILE = "testdata/items.txt"
+
+	// File containing patron ids; one per line. Assumed password: "pass".
+	PATRONSFILE = "testdata/patrons.txt"
+
+	// Maximum patron item checkouts per session
+	MAXCHEKCOUTS = 20
 )
 
-type RFIDService struct {
-	// TCP connection
-	conn net.Conn
-
-	// communication channels
-	incoming chan []byte
-	outgoing chan []byte
-	closing  chan bool
+type patron struct {
+	sync.Mutex
+	ID        string
+	Checkouts []string
 }
 
-func NewRFIDService() *RFIDService {
-	return &RFIDService{
-		incoming: make(chan []byte),
-		outgoing: make(chan []byte),
-		closing:  make(chan bool),
-	}
-}
+var (
+	patrons []*patron
+	items   []string
+)
 
-func (a *RFIDService) connect() error {
-	conn, err := net.Dial("tcp", HOST)
+func init() {
+	// load & init patrons
+	f1, err := os.Open(PATRONSFILE)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	a.conn = conn
-	return nil
+	defer f1.Close()
+	scanner := bufio.NewScanner(f1)
+	for scanner.Scan() {
+		patrons = append(patrons, &patron{ID: scanner.Text()})
+	}
+
+	// load items
+	f2, err := os.Open(ITEMSFILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+	reader2 := bufio.NewReader(f2)
+	contents, err := ioutil.ReadAll(reader2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	items = strings.Split(string(contents), "\n")
 }
 
-func (a *RFIDService) reader() {
-	r := bufio.NewReader(a.conn)
+func checkin(p *patron) {
+	p.Mutex.Lock()
+	defer p.Mutex.Unlock()
+	if len(p.Checkouts) == 0 {
+		log.Println("     patron has no checkouts")
+		return
+	}
+	var item string
+	for i := 0; i < rand.Intn(len(p.Checkouts)); i++ {
+		item, p.Checkouts = p.Checkouts[len(p.Checkouts)-1],
+			p.Checkouts[:len(p.Checkouts)-1]
+		log.Printf("     checkin item: %v\n", item)
+	}
+}
+
+func checkout(p *patron) {
+	p.Mutex.Lock()
+	defer p.Mutex.Unlock()
+	for i := 0; i < rand.Intn(MAXCHEKCOUTS); i++ {
+		item := items[rand.Intn(len(items))]
+		p.Checkouts = append(p.Checkouts, item)
+		log.Printf("     checkout item: %v\n", item)
+	}
+}
+
+func simulateAutomat(automat int) {
 	for {
-		msg, err := r.ReadBytes('\n')
-		if err != nil {
-			a.closing <- true
-		}
-		a.incoming <- msg
-	}
-}
-
-func (a *RFIDService) writer() {
-	w := bufio.NewWriter(a.conn)
-	for msg := range a.outgoing {
-		_, err := w.Write(msg)
-		if err != nil {
-			log.Println(err)
-		}
-		err = w.Flush()
-		if err != nil {
-			log.Println(err)
+		action := rand.Intn(100)
+		switch {
+		case action < 40 && action > 0:
+			patron := patrons[rand.Intn(len(patrons))]
+			log.Printf("[%d] simulating checkin patron: %v\n", automat, patron.ID)
+			checkin(patron)
+		case action > 40 && action < 80:
+			patron := patrons[rand.Intn(len(patrons))]
+			log.Printf("[%d] simulating login patron: %v\n", automat, patron.ID)
+			checkout(patron)
+		case action > 80:
+			log.Printf("[%d] simulating wait\n", automat)
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)+1))
 		}
 	}
 }
 
-func (a *RFIDService) run() {
-	defer a.conn.Close()
-
-	go a.reader()
-	go a.writer()
-
-	for {
-		select {
-		case msg := <-a.incoming:
-			log.Println("incoming", string(msg))
-		case <-a.closing:
-			log.Println("connection closing")
-			return
-		}
-
-	}
-
-}
-
-func TestTCPServer(t *testing.T) {
+func TestAutomatPatronInteraction(t *testing.T) {
 	s := specs.New(t)
 	server := newTCPServer(&config{TCPPort: "6666"})
+	rand.Seed(time.Now().UnixNano())
 
 	go server.run()
 	s.Expect(0, len(server.connections))
 
 	for i := 0; i < NUMCLIENTS; i++ {
-		c := NewRFIDService()
-		c.connect()
-		go c.run()
+		go simulateAutomat(i)
+
 	}
+
 	time.Sleep(DURATION)
 	s.Expect(NUMCLIENTS, len(server.connections))
 }
